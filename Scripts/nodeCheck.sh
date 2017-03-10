31a6d9111f4afeadd4665b91b95dc28047ecb26a
Trevor Scott <trevor@agree.com>
2017-03-10 15:17:43 -0800

    Fixing node and nvm install

--- Scripts/nodeCheck.sh
+++ Scripts/nodeCheck.sh
@@ -1,6 +1,7 @@
 if ! which npm; then
 	curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
-	. ~/.nvm/nvm.sh
+	export NVM_DIR="/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 	nvm install 6.10.0
 fi
 
