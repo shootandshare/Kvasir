d04dbb1b3e4f159123c711afdca8c2ab57035ee1
Trevor Scott <trevor@agree.com>
2017-03-10 15:08:38 -0800

    Added app spec file for AWS CodeDeploy

---
+++ Scripts/nodeCheck.sh
@@ -0,0 +1,6 @@
+if ! which npm; then
+	curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+	. ~/.nvm/nvm.sh
+	nvm install 6.10.0
+fi
+
---
+++ Scripts/start.sh
@@ -0,0 +1,2 @@
+cd /wepayAdminApp
+npm start
