ddbe1fd6ec04d85b8e5564712f65dca03c4cda15
Trevor Scott <trevor@agree.com>
2017-03-10 16:19:35 -0800

    use a different npm install

--- Scripts/nodeCheck.sh
+++ Scripts/nodeCheck.sh
@@ -1,5 +1,5 @@
 if ! which npm; then
-	curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+	curl -L https://www.npmjs.com/install.sh | sh
 	export NVM_DIR="/.nvm"
 	[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 	nvm install 6.10.0
