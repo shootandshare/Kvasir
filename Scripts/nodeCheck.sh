946558fb032f8c158aa9d7dc628559c39f69e7e6
Trevor Scott <trevor@agree.com>
2017-03-10 16:25:57 -0800

    Amazon recommended node version

--- Scripts/nodeCheck.sh
+++ Scripts/nodeCheck.sh
@@ -2,6 +2,6 @@ if ! which npm; then
 	curl -L https://www.npmjs.com/install.sh | sh
 	export NVM_DIR="/.nvm"
 	[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-	nvm install 6.10.0
+	nvm install 4.4.5
 fi
 
