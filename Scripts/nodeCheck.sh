26c759d2e38b729a7306c2866db67cd2ef7bb3bb
Trevor Scott <trevor@agree.com>
2017-03-13 16:59:25 -0700

    install npm as root

--- Scripts/nodeCheck.sh
+++ Scripts/nodeCheck.sh
@@ -1,9 +1,9 @@
 source /home/ec2-user/.bash_profile
 if ! which npm; then
-	curl -L https://www.npmjs.com/install.sh | sh
-	export NVM_DIR="/.nvm"
-	[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-	ln -s /usr/local/bin/npm /usr/bin/npm
-	nvm install 4.4.5
+	sudo curl -L https://www.npmjs.com/install.sh | sh
+	# export NVM_DIR="/.nvm"
+	# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+	sudo ln -s /usr/local/bin/npm /usr/bin/npm
+	npm install 4.4.5
 fi
 
