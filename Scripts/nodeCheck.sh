ae53da7efc0071e83f947338deb005fe26fafe3d
Trevor Scott <trevor@agree.com>
2017-03-13 17:08:11 -0700

    install npm as ec2-user with sudo

--- Scripts/nodeCheck.sh
+++ Scripts/nodeCheck.sh
@@ -1,9 +1,9 @@
 source /home/ec2-user/.bash_profile
 if ! which npm; then
-	sudo curl -L https://www.npmjs.com/install.sh | sh
+	sudo curl -L https://www.npmjs.com/install.sh | sudo sh
 	# export NVM_DIR="/.nvm"
 	# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 	sudo ln -s /usr/local/bin/npm /usr/bin/npm
-	npm install 4.4.5
+	sudo npm install 4.4.5
 fi
 
