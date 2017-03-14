e5fafd056c777eadd85750a13863100c15f1d749
Trevor Scott <trevor@agree.com>
2017-03-13 17:32:33 -0700

    use su -c instead of sudo

--- Scripts/nodeCheck.sh
+++ Scripts/nodeCheck.sh
@@ -1,10 +1,10 @@
-Defaults:ec2-user !requiretty
+Defaults:
 source /home/ec2-user/.bash_profile
 if ! which npm; then
-	sudo curl -L https://www.npmjs.com/install.sh | sudo sh
+	su -c curl -L https://www.npmjs.com/install.sh | su -c sh
 	# export NVM_DIR="/.nvm"
 	# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-	sudo ln -s /usr/local/bin/npm /usr/bin/npm
-	sudo npm install 4.4.5
+	su -c ln -s /usr/local/bin/npm /usr/bin/npm
+	su -c npm install 4.4.5
 fi
 
