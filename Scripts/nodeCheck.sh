f0c1424a16155a75a44b0e63bb14a29f7724054b
Trevor Scott <trevor@agree.com>
2017-03-13 17:57:28 -0700

    remove sudo again

--- Scripts/nodeCheck.sh
+++ Scripts/nodeCheck.sh
@@ -5,6 +5,6 @@ if ! which npm; then
 	# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 	# ln -s /usr/local/bin/npm /usr/bin/npm
 	# npm install 4.4.5
-	sudo apt-get -y install nodejs npm
+	apt-get -y install nodejs npm
 fi
 
