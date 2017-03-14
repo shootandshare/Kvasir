6369f1089935ca01df79d24339547be092101c58
Trevor Scott <trevor@agree.com>
2017-03-13 17:39:32 -0700

    sudo and su is not working

--- Scripts/nodeCheck.sh
+++ Scripts/nodeCheck.sh
@@ -1,9 +1,9 @@
 source /home/ec2-user/.bash_profile
 if ! which npm; then
-	su -c curl -L https://www.npmjs.com/install.sh | su -c sh
+	curl -L https://www.npmjs.com/install.sh | sh
 	# export NVM_DIR="/.nvm"
 	# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-	su -c ln -s /usr/local/bin/npm /usr/bin/npm
-	su -c npm install 4.4.5
+	ln -s /usr/local/bin/npm /usr/bin/npm
+	npm install 4.4.5
 fi
 
