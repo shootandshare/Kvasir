eb06ecca92447e39c5f06d8fbdf5f30bb7289cf0
Trevor Scott <trevor@agree.com>
2017-03-15 15:46:12 -0700

    stop iptables service

--- Scripts/nodeCheck.sh
+++ Scripts/nodeCheck.sh
@@ -13,8 +13,4 @@ if ! which npm; then
 	npm install forever-monitor -g
 fi
 
-# allow HTTP inbound and replies
-iptables -A INPUT -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 80 -m state --state ESTABLISHED -j ACCEPT
-# save config
-/sbin/service iptables save
+service iptables stop
